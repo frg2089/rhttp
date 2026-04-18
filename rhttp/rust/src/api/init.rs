ea8ea4a764475a7cf56e860d542185ed724df09d
Fichtelcoder <de.frankenapps@gmail.com>
2026-04-18 20:24:06 +0200

    feat: tone down logging

--- rhttp/rust/src/api/init.rs
+++ rhttp/rust/src/api/init.rs
@@ -1,5 +1,9 @@
 #[flutter_rust_bridge::frb(init)]
 pub fn init_app() {
-    // Default utilities - feel free to customize
-    flutter_rust_bridge::setup_default_user_utils();
+    flutter_rust_bridge::setup_log_to_console(if cfg!(debug_assertions) {
+        log::LevelFilter::Trace
+    } else {
+        log::LevelFilter::Warn
+    });
+    flutter_rust_bridge::setup_backtrace();
 }
